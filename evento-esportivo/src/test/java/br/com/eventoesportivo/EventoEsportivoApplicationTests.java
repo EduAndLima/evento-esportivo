382804e65e61a693c49ca5cd4e474b47c7a1cf46
DESKTOP-A7QM1N8\Eduardo Lima <eduandlima@gmail.com>
2023-12-01 17:38:06 -0300

    Criação do projeto

---
+++ evento-esportivo/src/test/java/br/com/eventoesportivo/EventoEsportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eventoesportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoEsportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
